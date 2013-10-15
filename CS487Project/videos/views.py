8bff4f22c3d489685f5b76b5afcfb98b948a01a7
Kristen Deppe <kristen.deppe@gmail.com>
2013-10-15 12:58:41 -0500

    Trying to figure out searching

--- CS487Project/videos/views.py
+++ CS487Project/videos/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from videos.models import Video
+from django.db.models import Count
 
 # Create your views here.
 class IndexView(generic.ListView):
@@ -26,6 +27,16 @@ def videoCount(request, pk):
 def searchResult(request):
         if 'query' in request.GET and request.GET['query']:
                 q = request.GET['query']
+		qlist = unicode.split(q)
+		print qlist
+		results = None
+		for query in qlist:
+			print query
+			if results is None:
+				results = Video.objects.filter(keywords=query)
+			else:
+				results = results | Video.objects.filter(keywords=query)
+		sresults = results.order_by(Count('keywords'))
                 return render(request, 'videos/search_results.html', {'query': q})
         else:
                 return render(request, 'videos/search.html')
