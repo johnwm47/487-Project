49a88ea404ad48d87dcaffcd339e5d1f843bb242
Peter Olson <polson2@hawk.iit.edu>
2013-10-16 10:07:36 -0500

    Changed file upload path. You should not need to do anything. Uploads already completed will be fine. New uploads will follow the new system.

--- CS487Project/videos/models.py
+++ CS487Project/videos/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from django.db import models
 import datetime
+from time import strftime
 
 # Create your models here.
 class Author(models.Model):
@@ -24,6 +25,11 @@ class Journal(models.Model):
         def __unicode__(self):
                 return "%s %s %s" % (self.name, self.year, self.edition)
 
+# instance: the model instance. In this case a Video object. The primary key probably
+# will not have been initialized yet, so instance.id cannot be assumed to exist.
+def filePath(instance, filename):
+        return strftime(instance.title + "-%y-%m-%d-%H-%M-%s.mp4")
+
 class Video(models.Model):
         title = models.CharField(max_length=100, unique=True)
         uploader = models.ForeignKey(User)
@@ -34,12 +40,12 @@ class Video(models.Model):
         authors = models.ManyToManyField(Author)
         keywords = models.ManyToManyField(Keyword)
         journal = models.ForeignKey(Journal)
-        video = models.FileField(upload_to='videos/')
+        video = models.FileField(upload_to=filePath)
 
         def __unicode__(self):
                 return self.title
+
 # comments
 # flags
 # ratings
 # related videos
-
