0a02cacbaee045e1508a0f21cddc2ae17635d2c2
Peter Olson <polson2@hawk.iit.edu>
2013-10-15 23:27:59 -0500

    Put views.py back in

---
+++ CS487Project/videos/views.py
@@ -0,0 +1,51 @@
+from django.contrib.auth.decorators import login_required, permission_required
+from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.utils.decorators import method_decorator
+from django.views import generic
+from videos.models import Video
+from django.db.models import Count
+
+# Create your views here.
+class IndexView(generic.ListView):
+        template_name = 'videos/index.html'
+        model = Video
+
+class VideoView(generic.DetailView):
+        template_name = 'videos/view.html'
+        model = Video
+
+class UploaderView(generic.TemplateView):
+	template_name = 'videos/upload.html'
+	model = Video
+
+        @method_decorator(permission_required('videos.add_video'))
+        def dispatch(self, *args, **kwargs):
+                return super(UploaderView, self).dispatch(*args, **kwargs)
+
+def videoCount(request, pk):
+        video = get_object_or_404(Video, pk=pk)
+        video.viewCount += 1
+        video.save()
+        return HttpResponse(status=200)
+
+def searchResult(request):
+        if 'query' in request.GET and request.GET['query']:
+                q = request.GET['query']
+		qlist = unicode.split(q)
+		print qlist
+		results = None
+		for query in qlist:
+			print query
+			if results is None:
+				results = Video.objects.filter(keywords__keyword=query)
+			else:
+				results = results | Video.objects.filter(keywords__keyword=query)
+		sresults = results.annotate(matches=Count('keywords')).order_by('-matches')
+		print sresults
+                return render(request, 'videos/search_results.html', {'query': q, 'results': sresults} )
+        else:
+                return render(request, 'videos/search.html')
+
+def uploadFile(request):
+	return render(None)
