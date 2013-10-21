71a3cab4b5fd0c30a3647fb7c0a2c4b600549dc9
Kristen Deppe <kristen.deppe@gmail.com>
2013-10-21 11:54:55 -0500

    made test_data

--- CS487Project/videos/testSearch.py
+++ CS487Project/videos/testSearch.py
@@ -1,18 +1,20 @@
 from django.test import TestCase
 from videos.models import Video
 from django.contrib.auth.models import User
+from django.test.client import RequestFactory
 
 class SearchTest(TestCase):
-	def SetUp(self):
-		os.system("../manage.py syncdb")
-		a = Author(name = "Bob", email = "bob@gmail.com")
-		a.save()
-		j = Journal(name = "test journal", year=1999, edition=1)
-		j.save()
-		k1 = Keyword(keyword = "test")
-		k2 = Keyword(keyword = "vid")
-		k1.save()
-		k2.save()
-		v = Video(title="testvid", uploader=User.objects.get(username="su"), description = "test video", url=http://google.com, journal
+	fixtures = ['test_data.json']
 
-	def SearchExists(self):
+	def SearchKeywordExists(self):		
+		r = self.client.get('/videos/search?query=test')
+		self.assertEqual(response.status_code, 200)
+		self.assertEqual(r.context['results'], Video.objects.filter(title="testvid"))
+		
+	def SearchKeywordDoesNotExist(self):
+		r = self.client.get('/videos/search?query=potato')
+		self.assertEqual(response.status_code, 200)
+		self.assertEqual(r.context['results'], [])
+
+	def SearchMultipleKeywords(self):
+		r = self.client.get('/videos/search?query=test+vid')
