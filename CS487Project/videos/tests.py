49fd2e990630fb864dccb8a68d7f596f7b360dc3
Kristen Deppe <kristen.deppe@gmail.com>
2013-10-21 12:51:14 -0500

    Merge fixed

--- CS487Project/videos/tests.py
+++ CS487Project/videos/tests.py
@@ -5,7 +5,9 @@ when you run "manage.py test".
 Replace this with more appropriate tests for your application.
 """
 
-from django.test import TestCase
+from django.test import TestCase, TransactionTestCase
+from models import Video
+from django.db.models import Count
 
 class UploadTest(TestCase):
         #fixtures = ['initial_data.json'] # create user accounts
@@ -22,3 +24,30 @@ class UploadTest(TestCase):
                 r = self.client.get('/videos/upload/')
                 self.assertEqual(r.status_code, 200)
 
+class SearchTest(TestCase):
+	fixtures = ['test_data.json']
+
+	def testSearchKeywordExists(self):		
+		r = self.client.get('/videos/search/?query=test')
+		self.assertEqual(r.status_code, 200)
+                print type(r.context['results'])
+                print type(Video.objects.filter(keywords__keyword='test'))
+                print type(r.context['results'][1])
+                print type(Video.objects.filter(keywords__keyword='test')[1])
+		self.assertQuerysetEqual(r.context['results'], Video.objects.filter(keywords__keyword='test'))
+		
+	def testSearchKeywordDoesNotExist(self):
+		r = self.client.get('/videos/search/?query=potato')
+		self.assertEqual(r.status_code, 200)
+                print type(r.context['results'])
+                print type(Video.objects.filter(keywords__keyword='potato'))
+		self.assertQuerysetEqual(r.context['results'], Video.objects.filter(keywords__keyword='potato'))
+
+	def testSearchMultipleKeywords(self):
+		r = self.client.get('/videos/search/?query=test+vid')
+		self.assertEqual(r.status_code, 200)
+                res = Video.objects.filter(keywords__keyword ='test') | Video.objects.filter(keywords__keyword = 'vid')
+                sres = res.annotate(matches=Count('keywords')).order_by('-matches')
+                print type(r.context['results'])
+                print type(sres)
+		self.assertQuerysetEqual(r.context['results'], sres)
