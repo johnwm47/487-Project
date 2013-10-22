4df634d06c3e7a7f17e16f4375ab4b65caee5fe2
dayu070 <dayu070@gmail.com>
2013-10-22 00:17:56 -0400

    Add two tests for new created user
    Jingyu

--- CS487Project/videos/jytests.py
+++ CS487Project/videos/jytests.py
@@ -14,3 +14,18 @@ class RegistrationTest(TestCase):
          r1 = self.client.get('/videos/upload/')
          self.assertEqual(r1.status_code, 302)
 
+    def test_register1(self):
+        request = self.client.post('/accounts/register/', {'username': 'test1', 'email': 'test1@hawk.iit.edu', 'password1': '123456', 'password2': '123456'})
+        self.assertTrue(self.client.login(username='test1', password='123456'))
+        r2 = self.client.get('/videos/upload/')
+        self.assertEqual(r2.status_code, 200)
+        r2 = self.client.get('/videos/search/')
+        self.assertEqual(r2.status_code, 200)
+
+    def test_register2(self):
+        request = self.client.post('/accounts/register/', {'username': 'test2', 'email': 'test2@hawk.iit', 'password1': '123456', 'password2': '123456'})
+        self.assertTrue(self.client.login(username='test2', password='123456'))
+        r3 = self.client.get('/videos/upload/')
+        self.assertEqual(r3.status_code, 302)
+        r3 = self.client.get('/videos/search/')
+        self.assertEqual(r3.status_code, 200)
